535c8c43a9cdabd5c901279d0bd5fc3119111142
MajaKarolina <maja.taegt@gmail.com>
2024-09-07 12:00:43 +0100

    Add project files.

---
+++ FlowerBouquet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstAppMaja
+{
+    internal class FlowerBouquet
+    {
+        public string flowerBouquet {  get; set; }
+        public int stems { get; set; } 
+        public double pricePerStem { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Linq;
+
+namespace FirstAppMaja
+{
+    internal class ReadInput
+    {
+        static void Main(string[] args)
+        {
+            string flowerBouqet;
+            int stems;
+            double price;
+
+            ReadInput(out flowerBouqet, out stems, out price);
+            Console.WriteLine($"\nflowerbouqet: {flowerBouqet}");
+            Console.WriteLine($"\nstems: {stems}");
+            Console.WriteLine($"\nstems: {price}");
+        }
+
+        static void ReadInput(out string flowerBouquet, out int stems, out double price);
+        Console.Write("Enter type of bouqet: ");
+            flowerBouqet = Console.ReadLine();
+
+    }
+}
